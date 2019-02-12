fbb3dcb695a3476db6e779c1aa05a37fcf45bf3b
grace-mao23 <gmao00@stuy.edu>
2019-02-12 10:16:50 -0500

    toString drafted

--- KnightBoard.java
+++ KnightBoard.java
@@ -11,23 +11,36 @@ public class KnightBoard {
 
 
   public String toString() {
-
+    String result = "";
+    for (int i = 0; i < board.length; i++) {
+      for (int x = 0; x < board[i].length; x++) {
+        if (board[i][x] < 10) {
+          result += " " + board[i][x] + " ";
+        } else {
+          result += board[i][x] + " ";
+        }
+      }
+      result += "\n";
+    }
+    return result;
   }
 
   //throws IllegalStateException when the board contains non-zero values.
   //throws IllegalArgumentException when either parameter is negative or out of bounds.
   public boolean solve(int startingRow, int startingCol) {
+    return true;
+  }
 
+  // level is the number of the knight
+  private boolean solveH(int row ,int col, int level) {
+    return true;
   }
 
   //throws IllegalStateException when the board contains non-zero values.
   //throws IllegalArgumentException when either parameter is negative or out of bounds.
   public int countSolutions(int startingRow, int startingCol) {
-
+    return -1;
   }
 
-  private boolean solveH(int row ,int col, int level) {
 
-  }
-  // level is the # of the knight
 }
