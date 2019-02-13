e7421908f959f72548e44708484788e81e99aea4
grace-mao23 <gmao00@stuy.edu>
2019-02-12 20:16:20 -0500

    Drafted private helper method to move knight

--- KnightBoard.java
+++ KnightBoard.java
@@ -25,6 +25,23 @@ public class KnightBoard {
     return result;
   }
 
+  // 0 - up left, 1 - up right, 2 - right up, 3 - right down, 4 - down right
+  // 5 - down left, 6 - right down, 7 - right up
+  private boolean moveKnight(int choice, int level, int row, int col) {
+    if (board[row][col] != -1) return false;
+    int[] rowChange = new int[] {-2, -2, -1, 1, 2, 2, 1, -1};
+    int[] colChange = new int[] {-1, 1, 2, 2, 1, -1, -2, -2};
+    int changedRow = row + rowChange[choice];
+    int changedCol = col + colChange[choice];
+    if (changedRow < 0 || changedCol < 0 ||
+        changedRow > board.length || changedCol > board[changedRow].length) {
+      return false;
+    }
+    board[row][col] = level;
+    board[changedRow][changedCol] = -1;
+    return true;
+  }
+
   //throws IllegalStateException when the board contains non-zero values.
   //throws IllegalArgumentException when either parameter is negative or out of bounds.
   public boolean solve(int startingRow, int startingCol) {
@@ -32,7 +49,7 @@ public class KnightBoard {
   }
 
   // level is the number of the knight
-  private boolean solveH(int row ,int col, int level) {
+  private boolean solveH(int row, int col, int level) {
     return true;
   }
 
@@ -44,7 +61,7 @@ public class KnightBoard {
 
   public static void main(String[] args) {
     KnightBoard k = new KnightBoard(2,2);
-    System.out.println()
+    System.out.println(k.toString());
   }
 
 
